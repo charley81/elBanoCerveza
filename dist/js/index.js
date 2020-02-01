b630a56628851011e185b0dbc51993c69b086c39
chrisharley81 <chrisharley81@gmail.com>
2020-01-31 19:24:23 -0500

    click off modal to close and esc key functionality and styling mobile

--- dist/js/index.js
+++ dist/js/index.js
@@ -9,15 +9,43 @@ function handleCardButtonClick(event) {
   const name = card.querySelector('h3').textContent;
   const cardParagraph = card.querySelector('p').textContent;
 
+  // add html img to inner modal
   modalInner.innerHTML = `
-    <img src='${imgSrc}'/>
+    <img width='300px' height='300px' src='${imgSrc}'/>
     <h3>${name}</h3>
     <p>${cardParagraph}</p>
   `;
 
+  // open modal
   modalOuter.classList.add('open');
 }
+
+// close modal
+function closeModal() {
+  modalOuter.classList.remove('open');
+}
+
+// handle click off
+function handleClickOff(event) {
+  const isOutSide = event.target.closest('.modal-inner');
+  if (!isOutSide) {
+    closeModal();
+  }
+}
+
+// listen for click off
+modalOuter.addEventListener('click', handleClickOff);
 
 buttons.forEach(button => {
   button.addEventListener('click', handleCardButtonClick);
 });
+
+// escModal
+function escModal(event) {
+  if (event.key === 'Escape') {
+    closeModal();
+  }
+}
+
+// listen for esc key
+window.addEventListener('keydown', escModal);
